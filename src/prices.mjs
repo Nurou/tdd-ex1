bb8f053aeb088ebc32b7627f47b8f6601c8ad97f
Joel Hassan <joel.nhn@gmail.com>
2022-04-03 00:34:58 +0300

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -18,12 +18,12 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parsePlainDate(req.query.date);
+    const date = parseDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
-  function parsePlainDate(dateString) {
+  function parseDate(dateString) {
     if (dateString) {
       return Temporal.PlainDate.from(dateString);
     }
